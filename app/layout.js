13d38a67341ed0f9a1b891bf3b820ca657ab5092
Sanyam2649 <sb.sanyambansal2003@gmail.com>
2025-12-02 16:07:06 +0530

    font-roboto

--- app/layout.js
+++ app/layout.js
@@ -4,15 +4,22 @@ import { usePathname } from 'next/navigation';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { UserProvider } from '@/contexts/UserContext';
+import { Roboto } from 'next/font/google';   
 import './globals.css';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],         // choose weights you need
+  variable: '--font-roboto',                    // optional CSS variable
+});
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
   const isProfilePage = /^\/(?:profile\/)?[a-f0-9]{24}$/i.test(pathname || '');
 
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={roboto.variable}>   {/* Add class here */}
+      <body className="font-roboto">               {/* Use font */}
         <UserProvider>
           {!isProfilePage && <Navbar />}
           {children}
